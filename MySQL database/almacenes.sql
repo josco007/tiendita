ded24ca811f92df29841068e3d0ddca92833e127
josco007 <noe.miranda.f@gmail.com>
2016-12-15 17:01:07 -0700

    clean database

---
+++ MySQL database/almacenes.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: select *
+from almacenes
+LIMIT 0, 1000
+
+-- Date: 2016-12-15 16:57
+*/
+INSERT INTO `almacenes` (`almacenid`,`nombre`,`descripcion`) VALUES (1,'Almacen General','Almacen General');
